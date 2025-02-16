8a0df3a4d2b649ce26837d58cc428262c73fdfea
emirarslan1 <emirarslan1@users.noreply.github.com>
2025-02-16 19:20:33 +0000

    deploy: 97a911bd47d616c4183fcfcdbe1fc1eadbfe015a

---
+++ location_script.js
@@ -0,0 +1,55 @@
+function getCoordinates() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const lat = position.coords.latitude.toString();
+                const lng = position.coords.longitude.toString();
+                console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+                resolve([lat, lng]);
+            },
+            (error) => {
+                console.warn(`ERROR(${error.code}): ${error.message}`);
+                reject(error);
+            },
+            { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+        );
+    });
+}
+
+// Step 2: Get city name from coordinates
+function getCity(coordinates) {
+    return new Promise((resolve, reject) => {
+        const [lat, lng] = coordinates;
+        const xhr = new XMLHttpRequest();
+        const L_API_KEY = import.meta.env.VITE_LOCATIONIQ_API_KEY;
+        const url = `https://us1.locationiq.com/v1/reverse.php?key=${L_API_KEY}&lat=${lat}&lon=${lng}&format=json`;
+
+        xhr.open("GET", url, true);
+        xhr.send();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    const response = JSON.parse(xhr.responseText);
+                    const city = response.address.province || response.address.city || response.address.town;
+                    console.log("City from API:", city);
+                    resolve(city);
+                } else {
+                    reject("Şehir bilgisi alınamadı.");
+                }
+            }
+        };
+    });
+}
+
+// Export function to get city name by location
+export default async function getCityNameByLocation() {
+    try {
+        const coordinates = await getCoordinates(); // Get coordinates
+        const city = await getCity(coordinates);  // Get city from API
+        return city;
+    } catch (error) {
+        console.error("Hata oluştu:", error);
+        return null;
+    }
+}
---
+++ script.js
@@ -0,0 +1,37 @@
+import getCityNameByLocation from "./location_Script.js";
+
+document.addEventListener("DOMContentLoaded", async function (){
+    const cityNameByLocation = await getCityNameByLocation();
+    const cityName = cityNameByLocation; //no need to use ?.value?.trim()
+
+    console.log("citynamee ", cityName);
+    if(!cityName){
+        console.error("Boş input girildi.");
+        return;
+    }
+
+    async function get_weather(city) {
+        const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+            .then(response => {
+                const hava_durumu = response.data.weather[0].description;
+                const temp = response.data.main.temp;
+                console.log("City: ", response.data.name);
+                // console.log(response.data.main.name); invalid
+                console.log("Hava Durumu: ", hava_durumu);
+                console.log("Derece (Celcius): ", temp);
+
+                document.getElementById("currCityh2").innerHTML = "Your City: " + response.data.name;
+                document.getElementById("weatherResult").innerHTML = hava_durumu + " & " + temp + " °C";
+                document.getElementById("iconId").src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
+            })
+            .catch(err => {
+                document.getElementById("weatherResult").innerHTML = "Şehir bulunamadı veya API hatası oluştu.";
+            });
+    }
+    
+    get_weather(cityName);
+});
+
+//aea
